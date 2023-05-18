7796249f8db0624c0ebfb3a6ceb36d946476e64d
Einha Alliah Muday <einhalliahmuday@gmail.com>
2023-05-18 19:43:37 +0800

    Created models and dummy data

---
+++ ProfileBusinessRules/DataLayer/InMemoryProfileData.cs
@@ -0,0 +1,82 @@
+﻿namespace DataLayer;
+
+public class InMemoryProfileData
+{
+	public List <ProfileAccount> profileAccounts = new List <ProfileAccount>();
+	
+	public void CreateProfileAccounts() 
+	{
+		ProfileAccount user = new ProfileAccount
+		{
+			name = "Juan Dela Cruz",
+			username = "juandelacruz",
+			genderPronouns = "he/him",
+			rating = "5.0 *****",
+			bio = "Nothing lasts forever, we can change the future.	- Alucard",
+			courYrSec = "BSIT 2-1",
+			location = "Binan City, Laguna"
+		};
+		
+		ProfileAccount profile2 = new ProfileAccount
+		{
+			name = "Einha Alliah Muday",
+			username = "einhaalliahgmuday",
+			genderPronouns = "she/her",
+			rating = "5.0 *****",
+			bio = "Live well.",
+			courYrSec = "BSIT 2-1",
+			location = "Binan City, Laguna"
+		};
+		
+		ProfileAccount profile3 = new ProfileAccount
+		{
+			name = "Danica Malana",
+			username = "BSIT2-1_DanicaMalana",
+			genderPronouns = "she/her",
+			rating = "5.0 *****",
+			bio = "227",
+			courYrSec = "BSIT 2-1",
+			location = "Cabuyao City, Laguna"
+		};
+		
+		ProfileAccount profile4 = new ProfileAccount
+		{
+			name = "Sarah Michelle Orejo",
+			username = "BSIT2-1_SarahMichelleOrejo",
+			genderPronouns = "she/her",
+			rating = "5.0 *****",
+			bio = "Sarah",
+			courYrSec = "BSIT 2-1",
+			location = "Binan City, Laguna"
+		};
+		
+		ProfileAccount profile5 = new ProfileAccount
+		{
+			name = "Andrea Balaba",
+			username = "BSIT2-1_AndreaBalaba",
+			genderPronouns = "she/her",
+			rating = "5.0 *****",
+			bio = "cute",
+			courYrSec = "BSIT 2-1",
+			location = "Binan City, Laguna"
+		};
+		
+		ProfileAccount profile6 = new ProfileAccount
+		{
+			name = "Razell Mae Quitalig",
+			username = "_RazellMaeQuitalig",
+			genderPronouns = "she/her",
+			rating = "5.0 *****",
+			bio = "rzllq",
+			courYrSec = "BSIT 2-1",
+			location = "Binan City, Laguna"
+		};
+		
+		profileAccounts.Add(user);
+		profileAccounts.Add(profile2);
+		profileAccounts.Add(profile3);
+		profileAccounts.Add(profile4);
+		profileAccounts.Add(profile5);
+		profileAccounts.Add(profile6);
+	}
+}
---
+++ ProfileBusinessRules/DataLayer/ProfileAccount.cs
@@ -0,0 +1,12 @@
+﻿namespace DataLayer;
+
+public class ProfileAccount
+{
+    public string name { get; set; }
+    public string username { get; set; }
+	public string genderPronouns { get; set; }
+	public string rating { get; set; }
+	public string bio { get; set; }
+	public string courYrSec { get; set; }
+	public string location { get; set; }
+}
