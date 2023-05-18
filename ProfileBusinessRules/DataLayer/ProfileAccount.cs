7f72fefeb36ba0ab7c1bc3bfda9d0eddcebaec97
Einha Alliah Muday <einhalliahmuday@gmail.com>
2023-05-19 00:14:02 +0800

    Created List for followers and following of profile accounts

--- ProfileBusinessRules/DataLayer/ProfileAccount.cs
+++ ProfileBusinessRules/DataLayer/ProfileAccount.cs
@@ -9,4 +9,7 @@ public class ProfileAccount
 	public string bio { get; set; }
 	public string courYrSec { get; set; }
 	public string location { get; set; }
+	
+	public List <ProfileAccount> followers = new List <ProfileAccount>();
+	public List <ProfileAccount> following = new List <ProfileAccount>();
 }
