56935bb1bb4131a70d63bff6bfa0377f287c1e56
Danica <danicamalana02@gmail.com>
2023-07-11 20:06:49 +0800

    Update RegistrationRules.cs

--- ProfileBusinessRules/RegistrationRules.cs
+++ ProfileBusinessRules/RegistrationRules.cs
@@ -1,63 +1,56 @@
-﻿using System;
-using ProfileDataLayer;
+﻿using ProfileDataLayer;
 using ProfileDataModels;
 
-namespace ProfileBusinessRules;
-
-public class RegistrationRules
+namespace ProfileBusinessRules
 {
-	InMemoryRegistrationData registrationData = new InMemoryRegistrationData();
-	InMemoryProfileData profileData = new InMemoryProfileData();
-	
-	public List<RegisteredAccount> GetAllRegisteredAccounts()
-	{
-		return registrationData.GetRegisteredAccounts();
-	}
-	
-	// DoesStudentExists in AllSISAccounts
-	
-	public bool DoesUsernameAlreadyExists(string username)
-	{
-		bool doesUsernameAlreadyExists = false;
-		var allRegisteredAccounts = GetAllRegisteredAccounts();
-		
-		foreach (var account in allRegisteredAccounts) {
-			if (account.username == username)
-			{
-				doesUsernameAlreadyExists = true;
-			}
-		}
-		
-		return doesUsernameAlreadyExists;
-	}
-	
-	public void RegisterProfileAccount(string studentNo, string username, string name, string courYrSec, string location)
-	{
-		registrationData.CreateRegisteredAccount(studentNo, username);
-		profileData.CreateProfileAccount(name, username, courYrSec, location);
-	}
-	
-	public bool IsAccountRegistered(string studentNo)
-	{
-		bool isAccountRegistered = false;
-		var allRegisteredAccounts = GetAllRegisteredAccounts();
-		
-		foreach (var account in allRegisteredAccounts)
-		{
-			if (account.studentNo == studentNo)
-			{
-				isAccountRegistered = true;
-			}
-		}
-		
-		return isAccountRegistered;
-	}
-	
-	
-	//Models: username, studentNo
-	//username, genderPronouns, dateJoined, bio, 	accountPrivacy
-	
-	//infos from SIS account: name (firstName, middleName, lastName), courYrSec, location
-	//following and followers
+    public class RegistrationRules
+    {
+        
+        ProfileDataService profileDataService = new ProfileDataService(); // Instantiate ProfileDataService
 
+        public List<RegisteredAccount> GetAllRegisteredAccounts()
+    {
+    return profileDataService.GetAllRegisteredAccounts();
+    }
+
+        public bool DoesUsernameAlreadyExists(string username)
+        {
+            bool doesUsernameAlreadyExists = false;
+            var allRegisteredAccounts = GetAllRegisteredAccounts();
+
+            foreach (var account in allRegisteredAccounts)
+            {
+                if (account.username == username)
+                {
+                    doesUsernameAlreadyExists = true;
+                }
+            }
+
+            return doesUsernameAlreadyExists;
+        }
+
+        public void RegisterProfileAccount(string studentNo, string username, string name, string courYrSec, string location)
+        {
+            registrationData.CreateRegisteredAccount(studentNo, username);
+            profileData.CreateProfileAccount(name, username, courYrSec, location);
+        }
+
+        public bool IsAccountRegistered(string studentNo)
+        {
+            bool isAccountRegistered = false;
+            var allRegisteredAccounts = GetAllRegisteredAccounts();
+
+            foreach (var account in allRegisteredAccounts)
+            {
+                if (account.studentNo == studentNo)
+                {
+                    isAccountRegistered = true;
+                }
+            }
+
+            return isAccountRegistered;
+        }
+
+
+    }
 }
